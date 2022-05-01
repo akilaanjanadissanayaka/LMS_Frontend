ead20aee8ba1328ec26964272d9f0c6c58eeed3e
Chathura <chathura.madura11@gmail.com>
2022-05-01 14:45:25 +0530

    add reusable side bar for users

--- lms/src/components/sidebar/Sidebar.jsx
+++ lms/src/components/sidebar/Sidebar.jsx
@@ -1,105 +1,41 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import "./sidebar.css";
 import { Link } from "react-router-dom";
-import {
-    LineStyle,
-    PermIdentity,
-    BarChart,
-    VerifiedUser,
-    MenuBook
-  } from "@material-ui/icons";
+import { SidebarDataAdmin,SidebarDataStudent,SidebarDataTeacher } from '../../data/Data';
+
 
 const Sidebar = () => {
+    const [selected, setSelected] = useState(0);
+
     return (
         
     <div className="sidebar">
     <div className="sidebarWrapper">
       <div className="sidebarMenu">
-        <h3 className="sidebarTitle">Menu</h3>
-        <ul className="sidebarList">
-          <Link to="/" className="link">
-          <li className="sidebarListItem active">
-            <LineStyle className="sidebarIcon" />
-            Dashboard
-          </li>
-          </Link>
-        </ul>
-      </div>
-      <div className="sidebarMenu">
-        <h3 className="sidebarTitle">Users</h3>
-        <ul className="sidebarList">
-          <Link to="/teachers" className="link">
-            <li className="sidebarListItem">
-              <PermIdentity className="sidebarIcon" />
-              Teachers
-            </li>
-          </Link>
-          <Link to="/students" className="link">
-            <li className="sidebarListItem">
-              <PermIdentity className="sidebarIcon" />
-              Students
-            </li>
-          </Link>
-          <Link to="/reports" className="link">
-          <li className="sidebarListItem">
-            <BarChart className="sidebarIcon" />
-            Reports
-          </li>
-          </Link>
-        </ul>
-      </div>
-      <div className="sidebarMenu">
-        <h3 className="sidebarTitle">Courses</h3>
-        <ul className="sidebarList">
-          <Link to="/g6" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 6
-          </li>
-          </Link>
-          <Link to="/g7" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 7
-          </li>
-          </Link>
-          <Link to="/g8" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 8
-          </li>
-          </Link>
-          <Link to="/g9" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 9
-          </li>
-          </Link>
-          <Link to="/g10" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 10
-          </li>
-          </Link>
-          <Link to="/g11" className="link">
-          <li className="sidebarListItem">
-            <MenuBook className="sidebarIcon" />
-            Grade 11
-          </li>
-          </Link>
-        </ul>
-      </div>
 
-      <div className="sidebarMenu">
-        <h3 className="sidebarTitle">Badges</h3>
+        <h3 className="sidebarTitle">Menu</h3>
+        
         <ul className="sidebarList">
-          <Link to="/ex" className="link">
-          <li className="sidebarListItem">
-            <VerifiedUser className="sidebarIcon" />
-            ex
+            {SidebarDataAdmin.map((item, index) => {
+                return (
+                    <div>
+          <Link to={item.linkto} className="link">
+          <li className={selected === index ? "sidebarListItem active" :"sidebarListItem"} 
+          key={index}
+          onClick={() => {
+            setSelected(index);
+          }}
+          >
+            <item.icon className="sidebarIcon" />
+            {item.heading}
           </li>
           </Link>
+          
+          </div>
+            );
+         })}
         </ul>
+   
       </div>
      
     </div>
---
+++ lms/src/data/Data.js
@@ -0,0 +1,149 @@
+import {
+    LineStyle,
+    PermIdentity,
+    BarChart,
+    VerifiedUser,
+    MenuBook,
+    Forum,
+    Storage
+  } from "@material-ui/icons";
+// Sidebar Data Admdin
+export const SidebarDataAdmin = [
+    {
+      icon: LineStyle,
+      heading: "Dashboard",
+      linkto:"/",
+    },
+    {
+      icon: PermIdentity,
+      heading: "Users",
+      linkto:"/users",
+    },
+    {
+      icon: MenuBook,
+      heading: "Subjects",
+      linkto:"/subjects",
+    },
+    {
+      icon: BarChart,
+      heading: "Reports",
+      linkto:"/reports",
+    },
+    {
+      icon: VerifiedUser,
+      heading: "Badges",
+      linkto:"/badges",
+    },
+  ];
+  // Sidebar Data Teacher
+export const SidebarDataTeacher = [
+    {
+      icon: LineStyle,
+      heading: "Dashboard",
+      linkto:"/",
+    },
+    {
+      icon: PermIdentity,
+      heading: "Session",
+      linkto:"/users",
+    },
+    {
+      icon: MenuBook,
+      heading: "My Subjects",
+      linkto:"/mysubjects",
+    },
+    {
+      icon: BarChart,
+      heading: "Review",
+      linkto:"/review",
+    },
+    {
+      icon: Forum,
+      heading: "Forum",
+      linkto:"/forum",
+    },
+    {
+        icon: VerifiedUser,
+        heading: "Leaderboard",
+        linkto:"/leaderboard",
+      },
+  ];
+  // Sidebar Data student
+export const SidebarDataStudent = [
+    {
+      icon: LineStyle,
+      heading: "Dashboard",
+      linkto:"/",
+    },
+    {
+      icon: Storage,
+      heading: "Session",
+      linkto:"/session",
+    },
+    {
+      icon: MenuBook,
+      heading: "My Subjects",
+      linkto:"/mysubjects",
+    },
+    {
+      icon: Forum,
+      heading: "Forum",
+      linkto:"/forum",
+    },
+    {
+      icon: VerifiedUser,
+      heading: "Leaderboard",
+      linkto:"/leaderboard",
+    },
+  ];
+//active users
+  export const userData = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
