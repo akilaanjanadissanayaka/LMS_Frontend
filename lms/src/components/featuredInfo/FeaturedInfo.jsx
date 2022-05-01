3b6f91864110ea0622a369e1748b6945061a51b9
Chathura <chathura.madura11@gmail.com>
2022-05-01 14:45:05 +0530

    create card component

---
+++ lms/src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,40 @@
+import "./featuredInfo.css";
+import React from "react";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+
+export default function FeaturedInfo() {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revanue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">2,4150.00rs</span>
+          <span className="featuredMoneyRate">
+            -11.4 <ArrowDownward  className="featuredIcon negative"/>
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Students</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">2500</span>
+          <span className="featuredMoneyRate">
+            -20 <ArrowDownward className="featuredIcon negative"/>
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+      <div className="featuredItem">
+        <span className="featuredTitle">Teachers</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">10</span>
+          <span className="featuredMoneyRate">
+            +2<ArrowUpward className="featuredIcon"/>
+          </span>
+        </div>
+        <span className="featuredSub">Compared to last month</span>
+      </div>
+    </div>
+  );
+}
