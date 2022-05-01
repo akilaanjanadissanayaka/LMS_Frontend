083502979824b1c52c109a35e15ff0720485751a
Chathura <chathura.madura11@gmail.com>
2022-05-01 09:11:50 +0530

    create topbar component

---
+++ lms/src/components/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './topbar.css';
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+        <div className="topbarWrapper">
+            <div className="topLeft">L</div>
+            <div className="topRight">R</div>
+        </div>
+        </div>
+    );
+};
+
+export default Topbar;
