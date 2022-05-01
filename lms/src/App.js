2e764185b820a4307aba5106942ea8e573781038
Chathura <chathura.madura11@gmail.com>
2022-05-01 14:44:34 +0530

    add routing links

--- lms/src/App.js
+++ lms/src/App.js
@@ -4,17 +4,26 @@ import {
   BrowserRouter,
   Routes, // instead of "Switch"
   Route,
-} from "react-router-dom";import Sidebar from "./components/sidebar/Sidebar";
+} from "react-router-dom";
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import User from "./pages/user/User";
+import Subject from "./pages/subject/Subject";
+import Report from "./pages/report/Report";
 function App() {
   return (
     <BrowserRouter>
-    <Topbar />
-    <Sidebar />
-      <Routes>
-        <Route exact path="/" element={""}></Route>
-        <Route path="/" element={""}></Route>
-      </Routes>
+      <Topbar />
+      <div className="container">
+      <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Dashboard/>}></Route>
+          <Route path="/users" element={<User/>}></Route>
+          <Route path="/subjects" element={<Subject/>}></Route>
+          <Route path="/reports" element={<Report/>}></Route>
+        </Routes>
+        </div>
     </BrowserRouter>
   );
 }
