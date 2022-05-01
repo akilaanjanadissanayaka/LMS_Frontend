534f08c07c7abb78007e163d743ca8d4ddd8df2c
Chathura <chathura.madura11@gmail.com>
2022-05-01 09:11:34 +0530

    remove unwanted files

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Topbar from './components/Topbar';
 function App() {
   return (
     <div className="App">
-      Start Development
+      <Topbar/>
     </div>
   );
 }
