dc83e059e5fc035aa71e414ae8e75d9b7b2bb8b6
Chathura <chathura.madura11@gmail.com>
2022-05-01 10:14:12 +0530

    add routing part

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import Topbar from './components/Topbar';
+import React from "react";
+import "./app.css";
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
 function App() {
   return (
-    <div className="App">
-      <Topbar/>
-    </div>
+    <BrowserRouter>
+    <Topbar />
+    <Sidebar />
+      <Routes>
+        <Route exact path="/" element={""}></Route>
+        <Route path="/" element={""}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
