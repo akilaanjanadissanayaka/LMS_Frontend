636abbf905761ee56041d15c534a9cc3d40e9240
Chathura <chathura.madura11@gmail.com>
2022-05-14 01:46:38 +0530

    add username to topBar

--- lms/src/components/topbar/Topbar.jsx
+++ lms/src/components/topbar/Topbar.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { fetchUserData } from "../../services/authenticationService";
 import "./topbar.css";
 import { NotificationsNone, ExitToApp, Settings } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
 const Topbar = () => {
+  const [data, setData] = useState({});
+
+  React.useEffect(() => {
+    fetchUserData()
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((e) => {
+        localStorage.clear();
+      });
+  }, []);
+
+  //log out and clear local storage
+  const logOut = () => {
+    localStorage.clear();
+    window.location.href = "/";
+    // props.history.push('/');
+  };
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -15,21 +34,20 @@ const Topbar = () => {
             <NotificationsNone />
             <span className="topIconBadge">5</span>
           </div>
-          <div className="topbarIconContainer">
+          {/* <div className="topbarIconContainer">
             <Settings />
-          </div>
+          </div> */}
           <div className="topbarIconContainer">
-            <ExitToApp />
+            <ExitToApp onClick={() => logOut()} />
           </div>
+          {data && `${data.firstName} ${data.lastName}`}
           <Link to="/profile">
-          <img
-            src=""
-            alt=""
-            className="topAvatar"
-          /></Link>
+            <img src="" alt="" className="topAvatar" />
+          </Link>
         </div>
       </div>
     </div>
+    
   );
 };
 
