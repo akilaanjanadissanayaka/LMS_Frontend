7e8181111301380ee04e94315214d3385384194e
Chathura <chathura.madura11@gmail.com>
2022-05-14 01:46:19 +0530

    add test login file

---
+++ lms/src/pages/testpage/TestPage.js
@@ -0,0 +1,55 @@
+import React,{useState} from 'react';
+import { Button, Container } from 'react-bootstrap';
+// import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import {fetchUserData} from '../../services/authenticationService';
+import { Link } from "react-router-dom";
+import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+const MainWrapper=styled.div`
+    padding-top:40px;`;
+
+export const TestPage=(props)=>{
+
+    // const dispatch=useDispatch();
+    // const [loading,setLoading]=useState(false);
+    const [data,setData]=useState({});
+
+    React.useEffect(()=>{
+        fetchUserData().then((response)=>{
+            setData(response.data);
+        }).catch((e)=>{
+            localStorage.clear();
+            // props.history.push('/');
+            window.location.href="/";
+        })
+    },[])
+
+    const logOut=()=>{
+
+        localStorage.clear();
+        window.location.href="/";
+        // props.history.push('/');
+
+    }
+
+    return (
+        
+        <Container>
+            <MainWrapper>
+                
+                <h4>Hello {data && `${data.firstName} ${data.lastName}`}</h4>
+                <br></br>
+                {data && data.roles && data.roles.filter(value => value.roleCode==='ADMIN').length>0 && <Button type="variant">Add User</Button> }
+                <br></br>
+                
+                <Button style={{marginTop:'5px'}} onClick={() =>logOut()}>Logout</Button>
+                {/* without using history props */}
+                <hr></hr>
+                {/* <Link to="/"><Button style={{marginTop:'5px'}}>Logout</Button></Link> */}
+            </MainWrapper>
+        </Container>
+    )
+}
+export default TestPage;
